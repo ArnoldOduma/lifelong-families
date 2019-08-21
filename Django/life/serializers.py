4b4da411a6daf4ac40355760af22f3b9d8049ad7
Tony Kihara <tonykiharatonkin@gmail.com>
2019-08-22 00:57:29 +0300

    added business and housing serializers.

--- Django/life/serializers.py
+++ Django/life/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Services
+from .models import Services,Business,Housing
 
-
+class BusinessSerialiser(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Business
+        fields = ('name','location','address','image','city','category','price','description','contact','available','verified')
+        
 class ServicesSerialiser(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Services
+        fields = ('name','location','address','image','city','category','price','description','contact','available','verified')
+
+class HousingSerialiser(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Housing
         fields = ('name','location','address','image','city','category','price','description','contact','available','verified')
