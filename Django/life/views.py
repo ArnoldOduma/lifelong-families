3a9eec53d5ee12e76214b9d843a291fa44526da7
Tony Kihara <tonykiharatonkin@gmail.com>
2019-08-22 16:58:45 +0300

    added the api views for the business,housing and services.

--- Django/life/views.py
+++ Django/life/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
 # Create your views here.
 from rest_framework import generics
-from .models import Services
-from .serializers import ServicesSerialiser
+from .models import Services,Business,Housing
+from .serializers import ServicesSerialiser,BusinessSerialiser,HousingSerialiser
 from rest_framework import viewsets
 
 class ServicesViewSet(viewsets.ModelViewSet):
 
     queryset = Services.objects.all()
     serializer_class = ServicesSerialiser
+
+class BusinessViewSet(viewsets.ModelViewSet):
+
+    queryset = Business.objects.all()
+    serializer_class = BusinessSerialiser
+    
+class HousingViewSet(viewsets.ModelViewSet):
+
+    queryset = Housing.objects.all()
+    serializer_class = HousingSerialiser
