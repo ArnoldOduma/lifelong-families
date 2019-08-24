4344a8b3b4cbc9d4a1b2341536a1741c1b5d4cd2
Tony Kihara <tonykiharatonkin@gmail.com>
2019-08-24 13:51:22 +0300

    added classes serialization fields.

--- Django/life/serializers.py
+++ Django/life/serializers.py
@@ -4,14 +4,14 @@ from .models import Services,Business,Housing
 class BusinessSerialiser(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Business
-        fields = ('name','location''','address','city','contact','description','category','verified')
+        fields = ('name','location','image','image1','image2','image3','image4','image5','address','city','contact','description','category','verified')
         
 class ServicesSerialiser(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Services
-        fields = ('name','location','address','image','city','category','price','description','contact','available','verified')
+        fields = ('name','location','address','image','image1','image2','image3','image4','image5','city','category','price','description','contact','available','verified')
 
 class HousingSerialiser(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Housing
-        fields = ('name','image','location','address','city','contact','description','verified')
+        fields = ('name','image','image1','image2','image3','image4','image5','location','address','city','contact','description','verified')
