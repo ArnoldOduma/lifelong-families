f459350ef6f6bea63b388c1993161632de952635
Arnold Oduma <arnoldcliff98@gmail.com>
2019-10-15 06:44:32 +0300

    This commit will create an auth guard to check login and logout

--- Angular/lifelongFamilies/src/app/app-routing.module.ts
+++ Angular/lifelongFamilies/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Error404Component } from './modules/user/components/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      enableTracing: true,
+      scrollPositionRestoration: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
